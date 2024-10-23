c98f1fc6cabb35241aec2171df36301751f51dd5
anmutetech <124485136+anmutetech@users.noreply.github.com>
2024-10-23 18:35:06 -0500

    Update main.tf

--- terraform/EC2_instance/main.tf
+++ terraform/EC2_instance/main.tf
@@ -2,15 +2,18 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "ec2_instance" {
-  ami           = var.ami_id
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id 
   instance_type = var.instance_type
+  key_name = var.instance_key
+  subnet_id              = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.sg.id]
 
   tags = {
-    Name = var.instance_name
+    Name = var.tags
   }
-}
 
-output "instance_id" {
-  value = aws_instance.ec2_instance.id
+  volume_tags = {
+    Name = var.tags
+  } 
 }
